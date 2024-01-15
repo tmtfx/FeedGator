8548213d31f8ec2e6e02d932cbd06d37f3ed9d0e
Fabio Tomat <f.t.public@gmail.com>
2024-01-15 10:01:44 +0100

    update install64.sh

--- install64.sh
+++ install64.sh
@@ -24,7 +24,10 @@ then
 	if ! [[ -e /boot/system/non-packaged/lib/python3.10/site-packages/Be ]]; then
 		mkdir /boot/system/non-packaged/lib/python3.10/site-packages/Be
 	fi
+	echo "copying compiled data to system folder..."
 	cd bin/x86_64/python3.10 && cp * /boot/system/non-packaged/lib/python3.10/site-packages/Be
+	ret7=$?
+	cd ../../..
 fi
 cd ..
 else
