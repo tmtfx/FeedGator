9f8fa6e5eb6e2441625885345788ac9af42eb6ff
Fabio Tomat <f.t.public@gmail.com>
2025-11-21 13:08:41 +0100

    initial fix for AboutWindow

--- FeedGator.py
+++ FeedGator.py
@@ -351,6 +351,7 @@ class AboutWindow(BWindow):
 		#self.AboutText.SetStylable(True)
 		stuff="FeedGator\n\nFeed our alligator with tasty newspapers!\n\nThis is a simple feed aggregator written in Python + Haiku-PyAPI and feedparser\n\nspecial thanks to coolcoder613eb and Zardshard\n\nFeedGator is a reworked update of BGator.\n\nVersion 2.1-beta\n\t\t\t\t\t\t\t\t\tby TmTFx\n\n\t\tpress ESC to close this window"
 		txtrun1=text_run()
+		print("dopo inizializzazione text_run")
 		txtrun1.offset=0
 		fon1=BFont(be_bold_font)
 		fon1.SetSize(48.0)
@@ -361,7 +362,7 @@ class AboutWindow(BWindow):
 		col1.blue=0
 		col1.alpha=200
 		txtrun1.color=col1
-		n=stuff.find("Feed our")
+		n=stuff.find("Feed our") # meglio usare qualcosa tipo find_byte in HaiPO
 		txtrun2=text_run()
 		txtrun2.offset=n
 		txtrun2.font=be_plain_font
@@ -371,10 +372,13 @@ class AboutWindow(BWindow):
 		col2.blue=0
 		col2.alpha=0
 		txtrun2.color=col2
-		arra=text_run_array()
-		arra.count=2
-		arra.runs[0]=txtrun1
-		arra.runs[1]=txtrun2
+		print("terminati text_runs")
+		arra=[txtrun1,txtrun2]
+		#arra=text_run_array()
+		#print("inizializzato text_run_array")
+		#arra.count=2
+		#arra.runs[0]=txtrun1
+		#arra.runs[1]=txtrun2
 		self.AboutText.SetText(stuff,arra)
 		self.box.AddChild(self.AboutText,None)
 
